700deb8c81d9280ff9c1b855e60445b88586f7a8
JJUEZ26 <johan26andres@hotmail.com>
2025-12-21 21:51:01 -0500

    Update view.js

--- src/library/view.js
+++ src/library/view.js
@@ -1,5 +1,3 @@
-import { initBeetle } from '../effects/beetle/index.js';
-3.  Busca la función donde se renderiza la librería (probablemente se llame `renderLibrary` o similar, o al final del `init`).
 import { getAppMode, state } from '../app/state.js';
 import { handlePageEffects } from '../effects/index.js';
 import { loadPageHistory } from '../utils/storage.js';
@@ -85,12 +83,4 @@ export async function openBook(bookData) {
     }
     switchToReaderView();
     if (goToPageRef) goToPageRef(state.currentStoryId, true);
-}
-// --- LÓGICA DEL ESCARABAJO ---
-// 30% de probabilidad de aparecer al entrar a la librería
-if (Math.random() < 0.3) {
-    // Pequeño retraso para no asustar al usuario inmediatamente
-    setTimeout(() => {
-        initBeetle();
-    }, 2000);
 }
