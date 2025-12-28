ecae0eda489b62f9b5ee0d1321b02211472a3868
JJUEZ26 <johan26andres@hotmail.com>
2025-12-28 01:12:55 -0500

    Update firebase-config.js

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -2,12 +2,12 @@ module.exports = (req, res) => {
     // Mantén las variables en el entorno de Vercel para no incluirlas en el repo.
     const firebaseConfig = {
         apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        authDomain: process.env.libreria-interactiva.firebaseapp.com,
+        projectId: process.env.libreria-interactiva,
+        storageBucket: process.env.libreria-interactiva.firebasestorage.app,
+        messagingSenderId: process.env.663772506106,
+        appId: process.env.1:663772506106:web:98db55443a6db7f5852643,
+        measurementId: process.env.G-0C50QQ119H
     };
 
     const requiredKeys = [
