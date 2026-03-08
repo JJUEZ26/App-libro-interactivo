07973c53bc5c13a8aefcc8aaf1af963af834a8ca
jjuez26 <johan26andres@hotmail.com>
2026-03-08 00:07:02 -0500

    fix: move service worker to public, bust cache to v5, include previous CSS fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lecturas-interactivas-cache-v4';
+const CACHE_NAME = 'lecturas-interactivas-cache-v5';
 
 // Recursos esenciales que se cachean al instalar
 const urlsToCache = [
