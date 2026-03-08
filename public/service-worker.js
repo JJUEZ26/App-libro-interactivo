104f247233e108590ade52a61006b290752d3971
jjuez26 <johan26andres@hotmail.com>
2026-03-08 12:50:06 -0500

    Restore circular aesthetic with Bukowski influences and properly crop image

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lecturas-interactivas-cache-v7';
+const CACHE_NAME = 'lecturas-interactivas-cache-v8';
 
 // Recursos esenciales que se cachean al instalar
 const urlsToCache = [
