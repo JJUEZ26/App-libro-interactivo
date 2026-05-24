98b58a2dabf767f7b46dcd7e7b1ff6b7274e24bf
jjuez26 <johan26andres@hotmail.com>
2026-05-24 13:38:32 -0500

    feat: slow down blood drip animation from 5.5s to 8.5s for more solemnity

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lecturas-interactivas-cache-v19';
+const CACHE_NAME = 'lecturas-interactivas-cache-v20';
 
 // Recursos esenciales que se cachean al instalar
 const urlsToCache = [
